4ae163ae6b2680710ea2a3c2a1e4cb0cdabd1ad0
lazzzykim <33sunghyeon@gmail.com>
2025-10-15 23:40:16 +0900

    예외 기본

--- src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -19,6 +19,7 @@ public class UncheckedTest {
         Assertions.assertThatThrownBy(() -> service.callThrow())
                 .isInstanceOf(MyUncheckedException.class);
     }
+
     // RuntimeException 상속받은 예외는 언체크 예외
     static class MyUncheckedException extends RuntimeException {
         public MyUncheckedException(String message) {
